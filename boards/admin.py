08ca72addfafb95aeea4f6952d398e0b1fce3a60
Angel Trejo <gu24ie0002@globaluniversity.edu.mx>
2026-04-20 23:26:09 -0600

    feat: improve admin panel with list_display, search_fields and list_filter

--- boards/admin.py
+++ boards/admin.py
@@ -1,21 +1,37 @@
 from django.contrib import admin
-from .models import Board, List, Card, Comment
+from .models import Workspace, Board, List, Card, Comment
+
+
+@admin.register(Workspace)
+class WorkspaceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'owner', 'created_at']
+    search_fields = ['name', 'owner__username']
+    list_filter = ['created_at']
+
 
 @admin.register(Board)
 class BoardAdmin(admin.ModelAdmin):
-    search_fields = ['name']
+    list_display = ['id', 'name', 'owner', 'workspace', 'color', 'created_at']
+    search_fields = ['name', 'owner__username']
+    list_filter = ['workspace', 'created_at']
+
 
 @admin.register(List)
 class ListAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'board', 'position']
+    search_fields = ['name', 'board__name']
     list_filter = ['board']
-    search_fields = ['name']
+
 
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
-    list_filter = ['list', 'assigned_to']
-    search_fields = ['title']
+    list_display = ['id', 'title', 'list', 'assigned_to', 'position', 'created_at']
+    search_fields = ['title', 'description']
+    list_filter = ['list', 'assigned_to', 'created_at']
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_filter = ['user']
-    search_fields = ['content']
+    list_display = ['id', 'user', 'card', 'created_at']
+    search_fields = ['content', 'user__username', 'card__title']
+    list_filter = ['user', 'created_at']
